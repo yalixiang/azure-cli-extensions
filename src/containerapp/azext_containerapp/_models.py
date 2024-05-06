847c8fad283c4e086c5514342bcfb65cb5e1208b
Stacy Zeng <stacyzeng@microsoft.com>
2024-05-06 16:43:56 -0700

    code inspector crud

--- src/containerapp/azext_containerapp/_models.py
+++ src/containerapp/azext_containerapp/_models.py
@@ -313,6 +313,16 @@ SessionPool = {
     }
 }
 
+SessionCodeInterpreterPythonExecution = {
+    "properties": {
+        "identifier": None,
+        "codeInputType": None,
+        "executionType": None,
+        "pythonCode": None,
+        "timeoutInSeconds": None
+    }
+}
+
 DaprComponentResiliency = {
     "properties": {
         "inboundPolicy": {
