9f5a15275c000295602796bc1447d271321c47eb
Stacy Zeng <stacyzeng@microsoft.com>
2024-05-10 16:43:35 -0700

    fix help file

--- src/containerapp/azext_containerapp/_help.py
+++ src/containerapp/azext_containerapp/_help.py
@@ -1719,5 +1719,5 @@ helps['containerapp session code-interpreter list-files'] = """
     examples:
     - name: List files uploaded in a code-interpreter session.
       text: |
-          az containerapp sessionpool list-files -n MySessionPool -g MyResourceGroup --identifier MySession --path /example
+          az containerapp sessionpool list-files -n MySessionPool -g MyResourceGroup --identifier MySession
 """
